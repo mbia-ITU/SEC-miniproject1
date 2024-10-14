513403f4d286ab4b0c9054117f55f10fa9cafa64
Mikkel Bistrup Andersen <mbia@itu.dk>
2024-10-14 14:39:36 +0200

    fixed assignment

--- src/main.go
+++ src/main.go
@@ -22,7 +22,7 @@ func main() {
 	bobSecret, msg := interceptmsg(*bobPKey, *alicePKey, *msgc)
 	fmt.Printf("Secret is: %s, Message is: %s\n", bobSecret.Text(10), msg.Text(10))
 
-	modifiedmsgc := big.NewInt(6000)
+	modifiedmsgc := msgc.Mul(big.NewInt(3), msgc)
 	bobMsg := elgamelDecrypt(bobSecret, *alicePKey, *modifiedmsgc)
 	fmt.Printf("Tampered message: %s\n", bobMsg.Text(10))
 }
